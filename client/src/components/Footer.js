5fafa4ee15d156fbfba4f3785b2a616ddc2d8d06
RoberFonseca <roberfonseca944@gmail.com>
2023-01-10 19:38:58 -0300

    added mongodb, and replaced the local storage for files

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,14 +1,16 @@
-import { Footer as ArwesFooter, Paragraph } from "arwes";
-import Centered from "./Centered";
+import { Footer as ArwesFooter, Paragraph } from 'arwes';
+import Centered from './Centered';
 
 const Footer = () => {
-  return <ArwesFooter animate>
-    <Centered>
-      <Paragraph style={{ fontSize: 14, margin: "10px 0" }}>
-        Te amo leo, por favor juga el proximomundial.
-      </Paragraph>
-    </Centered>
-  </ArwesFooter>
+  return (
+    <ArwesFooter animate>
+      <Centered>
+        <Paragraph style={{ fontSize: 14, margin: '10px 0' }}>
+          Te amo leo, por favor juga el proximo mundial.
+        </Paragraph>
+      </Centered>
+    </ArwesFooter>
+  );
 };
 
 export default Footer;
--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const { start } = require('repl');
+const mongoose = require('mongoose');
 
 const app = require('./app.js');
 
@@ -7,9 +7,21 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+  'mongodb+srv://nasa-api:AFDXXsgZ5b8ybbG0@nasacluster.umbf8hu.mongodb.net/nasa?retryWrites=true&w=majority';
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+  console.log('MongoDB ta listo!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB tiene un error! =>', err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
