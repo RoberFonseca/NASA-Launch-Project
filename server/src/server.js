37078ba913a51b169b06a25b2bbad3ca802148f7
RoberFonseca <roberfonseca944@gmail.com>
2023-01-11 00:37:26 -0300

    added spacexapi launches + env file

--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,21 @@
 const http = require('http');
-const { mongoConnect } = require('./services/mongo');
+
+require('dotenv').config();
 
 const app = require('./app.js');
 
+const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
+const { loadLaunchData } = require('./models/launches.model');
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
 async function startServer() {
-  await mongoConnect;
+  await mongoConnect();
   await loadPlanetsData();
+  await loadLaunchData();
 
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}..`);
--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL =
-  'mongodb+srv://nasa-api:AFDXXsgZ5b8ybbG0@nasacluster.umbf8hu.mongodb.net/nasa?retryWrites=true&w=majority';
+require('dotenv').config();
+
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB ta listo!');
